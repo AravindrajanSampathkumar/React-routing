bfeb22c807f85396b84fe68d5136257c43b230d2
Aravindrajan_Sampath <aravindrajan_sampath@dellteam.com>
2021-12-01 08:21:27 +0530

    with HTTP

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,16 +1,28 @@
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../quotes/QuoteForm";
+import useHttp from "../../hooks/use-http";
+import { addQuote } from "../../lib/api";
+import { useEffect } from "react";
 
 const NewQuotes = () => {
+    const { sendRequest, status } = useHttp(addQuote);
     const history = useHistory();
+
+    useEffect(() => {
+        if (status === 'completed') {
+        history.push('/quotes');
+        }
+    }, [status,history]);
+
     const addQuoteHandler = (quoteData) => {
         console.log('quoteData=', quoteData);
+        sendRequest(quoteData);
         history.push('/quotes')// push is used for navigate to move next page and coming back to previous page 
         // history.replace('/quotes') // replace will change the path not able to move back
         //history allows us to change and manage history object and history URL
     }
     return (
-        <QuoteForm onAddQuote={addQuoteHandler}></QuoteForm>
+        <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler}></QuoteForm>
     )
 }
 
